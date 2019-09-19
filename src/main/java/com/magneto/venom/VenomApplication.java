fde6795060e9c62d441d2588e2ca879ecfbd48e2
yahto <yantong@nibirutech.com>
2019-09-19 09:59:39 +0800

    不多bb,先启一个springboot😊

---
+++ src/main/java/com/magneto/venom/VenomApplication.java
@@ -0,0 +1,15 @@
+package com.magneto.venom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yahto
+ * @date 2019/9/19 9:53 AM
+ */
+@SpringBootApplication
+public class VenomApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VenomApplication.class, args);
+    }
+}
